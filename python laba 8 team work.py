f1dbcf549e88a06579afb962081d5b532cd34385
dinoriko <152818550+dinoriko@users.noreply.github.com>
2024-11-19 20:52:40 +0200

    Update python laba 8 team work.py

--- python laba 8 team work.py
+++ python laba 8 team work.py
@@ -80,3 +80,24 @@ def display_students():
         print("Оцінки:")
         for subject, grade in info['grades'].items():
             print(f"  {subject}: {grade}")
+
+# (Чорномаз Вікторія КН-33.1)
+# Функція для редагування оцінок студента
+def edit_grades():
+    pib = input("Введіть ПІБ студента, оцінки якого потрібно змінити: ")
+    if pib not in students:
+        print(f"Студента '{pib}' не знайдено.")
+        return
+
+    grades = students[pib]["grades"]
+    for subject in grades:
+        try:
+            new_grade = int(input(f"Введіть нову оцінку за '{subject}' (1-5): "))
+            if 1 <= new_grade <= 5:
+                grades[subject] = new_grade
+            else:
+                print("Оцінка повинна бути в діапазоні від 1 до 5.")
+        except ValueError:
+            print("Оцінка повинна бути числом.")
+
+    print(f"Оцінки студента '{pib}' оновлено.")
